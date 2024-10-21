91ed6988dc746eb6356cbf7ddf1b50d5d8aaf01a
Ramneek Singh <singhrramneek@gmail.com>
2024-10-21 17:35:38 +0530

    Frontend: Tailwind,TS added, Backend: mysql, sequalize added, basic db config added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 2000
+const cors = require('cors')
 
+app.use(cors())
+
+app.use(express.json());
+
+app.get("/",(req, res) => {
+  res.send({message:"Hello world"})
+})
 app.get('/ping', (req, res) => {
   res.send('Pong!')
 })
