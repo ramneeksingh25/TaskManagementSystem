7e074f8736c9a35b18b4e5103d0fabb8fac51960
Ramneek Singh <singhrramneek@gmail.com>
2024-10-24 18:41:27 +0530

    added Dark Mode Theme switch button

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext.tsx";
+import ThemeProvider from "./context/ThemeContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AuthProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    </AuthProvider>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<ThemeProvider>
+			<AuthProvider>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</AuthProvider>
+		</ThemeProvider>
+	</StrictMode>
+);
---
+++ client/src/pages/Dashboard/ThemeButton.tsx
@@ -0,0 +1,15 @@
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useTheme } from "../../context/ThemeContext";
+
+const ThemeButton = () => {
+	const { toggleTheme, theme } = useTheme();
+	return (
+		<div
+			className="absolute bottom-1 left-1 w-fit rounded-full cursor-pointer hover:underline hover:text-white hover:dark:bg-indigo-500 dark:text-indigo-500 hover:bg-orange-400 text-orange-400 bg-zinc-900 transition-colors duration-150 p-2 border border-orange-400 dark:border-indigo-500"
+			onClick={toggleTheme}>
+			{theme == "dark" ? <FaMoon /> : <FaSun />}
+		</div>
+	);
+};
+
+export default ThemeButton;
